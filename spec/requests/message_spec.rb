a8f280954109ba76f7b7c23369d45e9513131d21
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-06-06 13:34:03 -0400

    fixing tests

--- spec/requests/message_spec.rb
+++ spec/requests/message_spec.rb
@@ -4,7 +4,7 @@ describe "Message", type: :api do
   let!(:room) { FactoryGirl.create(:room) }
   it "creates a message" do
     Net::HTTP.any_instance.stub(:post_form).and_return(true)
-    post messages_path(:format => "json"), body: { content: "hello", room_id: room.id }.to_json
+    post messages_path(:format => "JSON"), message: { content: "hello", room_id: room.id }.as_json
     response.status.should == 201
   end
 end
