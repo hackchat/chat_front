bf1deff4c7045cb49743f72954cbd000d09d8399
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-06-07 15:49:57 -0400

    modify unicorn.rb file

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
-root = "/home/deployer/apps/chat_front/current/public"
+root = "/home/deployer/apps/chat_front/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+stderr_path "#{root}/log/unicorn_chat_front.log"
+stdout_path "#{root}/log/unicorn_chat_front.log"
 
 listen "/tmp/unicorn.chat_front.sock"
 listen 7000
