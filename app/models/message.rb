8f7410e4d3e603cac3b6eee272c0d32284fc42e2
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-06-09 23:13:39 -0400

    refactoring of redis

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,5 @@
 class Message < ActiveRecord::Base
   attr_accessible :content, :room_id
-  # after_create :broadcast_temp
-  REDIS_URI = URI.parse(REDIS_URL)
-  REDIS = Redis.new(host: REDIS_URI.host, port: REDIS_URI.port, password: REDIS_URI.password)
 
   def self.broadcast_creation(params)
     REDIS.publish("create", self.build_redis_hash(params[:content], params[:room_id]))
