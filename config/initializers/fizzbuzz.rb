c2c1d758b3deb935b87b9acb43ecf37cb17cb8a2
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-06-18 00:12:45 -0400

    removing fizzbuz

--- config/initializers/fizzbuzz.rb
+++ config/initializers/fizzbuzz.rb
@@ -1,9 +1,9 @@
-# For emergency use only.
+# # For emergency use only.
 
-100.times do |n|
-  str = ''
-  str += 'Fizz' if (n % 3 == 0)
-  str += 'Buzz' if (n % 5 == 0)
-  str = n if (str == '')
-  puts str
-end
+# 100.times do |n|
+#   str = ''
+#   str += 'Fizz' if (n % 3 == 0)
+#   str += 'Buzz' if (n % 5 == 0)
+#   str = n if (str == '')
+#   puts str
+# end
